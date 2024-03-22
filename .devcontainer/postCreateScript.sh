52b54c82145a1924a797957ab33433e5b6a776e9
JoseMokeni <94537496+JoseMokeni@users.noreply.github.com>
2024-03-22 22:11:24 +0000

    updated

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -1,4 +1,4 @@
 conda init
 source ~/.bashrc
-conda create -y -n DL numpy pandas matplotlib tensorflow scikit-learn theano keras pytorch seaborn nltk cryptography
+conda create -y -n DL numpy pandas matplotlib tensorflow scikit-learn theano keras pytorch seaborn
 conda activate DL
